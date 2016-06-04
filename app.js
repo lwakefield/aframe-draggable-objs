0b7a5962353724e6f3a79dbe87155173a56fe3c0
Lawrence Wakefield <lawrence@iamlawrence.me>
2016-06-04 14:31:36 -0400

    more accurate clicking and preserves distance when dragging

--- app.js
+++ app.js
@@ -3,27 +3,32 @@ AFRAME.registerComponent('draggable', {
         this.mouse = new THREE.Vector2();
         this.scene = this.el.sceneEl;
         this.camera = this.scene.camera;
+        this.obj = this.el.object3D;
 
-        document.addEventListener('mousemove', e => {
-            this.mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-            this.mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+        this.scene.addEventListener('mousemove', e => {
+            this.mouse.x = ( e.offsetX / this.scene.canvas.offsetWidth ) * 2 - 1;
+            this.mouse.y = - ( e.offsetY / this.scene.canvas.offsetHeight ) * 2 + 1;
 
             if (this.selected) {
                 let r = new THREE.Raycaster();
                 r.setFromCamera(this.mouse, this.camera);
-                let point = r.ray.direction.multiplyScalar(5);
+                let dist = this.obj.position.distanceTo(this.camera.position);
+                let point = r.ray.direction.multiplyScalar(dist);
                 this.el.setAttribute('position', `${point.x} ${point.y} ${point.z}`);
             }
         });
 
-        document.addEventListener('mousedown', e => {
+        this.scene.addEventListener('mousedown', e => {
             let r = new THREE.Raycaster();
             r.setFromCamera( this.mouse, this.camera );
             let intersected = r.intersectObject( this.el.object3D, true );
+            let objPos = this.el.object3D.position;
+            let camPos = this.camera.position;
+            console.log(objPos.distanceTo(camPos));
             if (intersected.length) this.selected = true;
         });
 
-        document.addEventListener('mouseup', e => {
+        this.scene.addEventListener('mouseup', e => {
             this.selected = undefined;
         });
     }
